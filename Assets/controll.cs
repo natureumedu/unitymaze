4b8713a69133376993d220e9a478673e7a9e75cd
natureumedu <julyumedu@gmail.com>
2015-06-05 18:26:33 +0900

    ゴール時のアクションを追加

---
+++ Assets/controll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityChan;
+
+public class controll : MonoBehaviour {
+	Animator anime;
+	UnityChanControlScriptWithRgidBody unitychan;
+	// Use this for initialization
+	void Start () {
+		anime = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGoal(){
+		unitychan.enabled = false;
+		anime.SetBool ("Win", true);
+	}
+}
